06f97396b01f7e6cec071cf646521673e244b9cb
Georgyyy <neculageorgiana285@gmail.com>
2024-09-12 13:11:51 +0300

    first route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // TEST
     return view('welcome');
+});
+Route::get('/about', function () {
+    return view('about');
 });
