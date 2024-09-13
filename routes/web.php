80a9184331e12f6bc3b32c22f6ff16fccda76de3
Georgyyy <neculageorgiana285@gmail.com>
2024-09-13 15:22:38 +0300

    Lesson 9

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
 Route::get('/jobs', function () {
     return view('jobs', [
         'jobs' => Job::all()
