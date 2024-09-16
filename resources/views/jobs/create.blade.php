e52b544fc722ca0ef9b3856ba39823d86957cbdb
Georgyyy <neculageorgiana285@gmail.com>
2024-09-16 11:00:11 +0300

    Lesson 24

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -17,12 +17,12 @@
                         <div class="mt-2">
                         <x-form-input name="title" id="title" placeholder="CEO" />
 
-                            x-form-error name="title" />
+                            <x-form-error name="title" />
                         </div>
-                        </x-form-field>
+                </x-form-field>
 
-                            <x-form-field>
-                        <x-form-label for="salary">Salary</x-form-label>
+                    <x-form-field>
+                            <x-form-label for="salary">Salary</x-form-label>
                         <div class="mt-2">
                         <x-form-input name="salary" id="salary" placeholder="$50,000 USD" />
 
